05355824c428ad3cf0a0a60fa7896e6bfcb8c0ba
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-11 12:00:55 -0300

    más simple, sin puntos de montaje

--- create-php-jail.sh
+++ create-php-jail.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NAME=$1
-DIRECTORY=/srv/jails/$NAME
+DIRECTORY=/srv/http/$NAME
 
 if [ -e $DIRECTORY ]; then
 	echo "Directory $DIRECTORY exists" > /dev/errout
@@ -9,40 +9,21 @@ if [ -e $DIRECTORY ]; then
 fi
 
 echo "Creando arbol"
-
-mkdir -p $DIRECTORY/{etc,tmp,usr,var,dev}
-mkdir -p $DIRECTORY/var/{www,cache,tmp,lib}
-mkdir -p $DIRECTORY/usr/share
+mkdir -p $DIRECTORY/{etc,tmp,usr,var,dev} \
+         $DIRECTORY/var/{www,cache,tmp,lib} \
+         $DIRECTORY/usr/share
 
 echo "Permisos"
-
 chmod a+rw -R $DIRECTORY/tmp
 chmod a+rw -R $DIRECTORY/var/{tmp,cache}
 
-echo "Copiando timezones"
-
-cp -r /usr/share/zoneinfo $DIRECTORY/usr/share
-
-echo "Setting timezone as UTC"
-
-cd $DIRECTORY/etc 
-
-ln -s ../usr/share/zoneinfo/UTC localtime
+echo "Timezone"
+cp --dereference /etc/localtime ${DIRECTORY}/etc/
 
 echo "Creating nodes"
-
 mknod $DIRECTORY/dev/null c 1 3
 mknod $DIRECTORY/dev/random c 1 8
 mknod -m 644 $DIRECTORY/dev/urandom c 1 9
 chmod 666 $DIRECTORY/dev/{null,random,urandom}
 
-echo "mounting directory /var/www/$NAME"
-
-if [ ! -d /var/www/$NAME ]; then
-    mkdir -p /var/www/$NAME
-fi
-
-echo "# mount www directory on jail for $NAME" >> /etc/fstab
-echo "/var/www/$NAME $DIRECTORY/var/www none bind" >> /etc/fstab
-
-mount -a
+echo "Done"
