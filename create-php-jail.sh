c1a21e75711dd78ea91ab3b9445534c2bb172f7a
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-11 12:05:07 -0300

    chequeos

--- create-php-jail.sh
+++ create-php-jail.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+# salir al primer error
+set -e
 
 NAME=$1
 DIRECTORY=/srv/http/$NAME
 
-if [ -e $DIRECTORY ]; then
-	echo "Directory $DIRECTORY exists" > /dev/errout
-	exit 1
-fi
+# la jaula ya existe
+test -f "${DIRECTORY}/.jail" && exit 1
 
-echo "Creando arbol"
+echo "Arbol"
 mkdir -p $DIRECTORY/{etc,tmp,usr/bin,dev,srv}
 
 echo "Permisos"
 chmod a+rw -R $DIRECTORY/tmp
-chmod a+rw -R $DIRECTORY/var/{tmp,cache}
 
 echo "Timezone"
 cp --dereference /etc/localtime ${DIRECTORY}/etc/
 
-echo "Creating nodes"
+echo "Nodos"
 mknod $DIRECTORY/dev/null c 1 3
 mknod $DIRECTORY/dev/random c 1 8
 mknod -m 644 $DIRECTORY/dev/urandom c 1 9
 chmod 666 $DIRECTORY/dev/{null,random,urandom}
+
+touch "${DIRECTORY}/.jail"
 
 echo "Done"
