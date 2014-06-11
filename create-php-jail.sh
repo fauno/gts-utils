47665fe9ef33e598d7d083c122e68c37941d953a
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-11 12:10:43 -0300

    copiar un passwd falso y usar menos $DIRECTORY

--- create-php-jail.sh
+++ create-php-jail.sh
@@ -8,21 +8,24 @@ DIRECTORY=/srv/http/$NAME
 # la jaula ya existe
 test -f "${DIRECTORY}/.jail" && exit 1
 
+pushd "${DIRECTORY}" &>/dev/null
+
 echo "Arbol"
-mkdir -p $DIRECTORY/{etc,tmp,usr/bin,dev,srv/http/${DIRECTORY}}
+mkdir -p {etc,tmp,usr/bin,dev,srv/http/${DIRECTORY}}
 
 echo "Permisos"
-chmod a+rw -R $DIRECTORY/tmp
+chmod a+rw -R tmp
 
 echo "Timezone"
-cp --dereference /etc/localtime ${DIRECTORY}/etc/
+cp --dereference /etc/localtime etc/
+cp data/passwd etc/
 
 echo "Nodos"
-mknod $DIRECTORY/dev/null c 1 3
-mknod $DIRECTORY/dev/random c 1 8
-mknod -m 644 $DIRECTORY/dev/urandom c 1 9
-chmod 666 $DIRECTORY/dev/{null,random,urandom}
+mknod dev/null c 1 3
+mknod dev/random c 1 8
+mknod -m 644 dev/urandom c 1 9
+chmod 666 dev/{null,random,urandom}
 
-touch "${DIRECTORY}/.jail"
+touch .jail
 
 echo "Done"
