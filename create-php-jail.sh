be38639579b6c44694692247ea903dad16620c58
Nicolás Reynolds <fauno@endefensadelsl.org>
2014-06-11 12:16:15 -0300

    los jails van dentro de directorios especiales

--- create-php-jail.sh
+++ create-php-jail.sh
@@ -6,12 +6,17 @@ NAME=$1
 DIRECTORY=/srv/http/$NAME
 
 # la jaula ya existe
-test -f "${DIRECTORY}/.jail" && exit 1
+test -d "${DIRECTORY}/jail" && exit 1
 
-pushd "${DIRECTORY}" &>/dev/null
+echo "Jail"
+mkdir -p "${DIRECTORY}/jail"
+pushd "${DIRECTORY}/jail" &>/dev/null
 
 echo "Arbol"
-mkdir -p {etc,tmp,usr/bin,dev,srv/http/${DIRECTORY}}
+mkdir -p {etc,tmp,usr/bin,dev,srv/http}
+# linkear / a /srv/http/dominio para no tener que trabajar con
+# /srv/http/dominio/srv/http/dominio en el host
+ln -s / "srv/http/${DIRECTORY}"
 
 echo "Permisos"
 chmod a+rw -R tmp
@@ -19,13 +24,12 @@ chmod a+rw -R tmp
 echo "Timezone"
 cp --dereference /etc/localtime etc/
 cp data/passwd etc/
+cp data/resolv.conf etc/
 
 echo "Nodos"
 mknod dev/null c 1 3
 mknod dev/random c 1 8
-mknod -m 644 dev/urandom c 1 9
+mknod dev/urandom c 1 9
 chmod 666 dev/{null,random,urandom}
-
-touch .jail
 
 echo "Done"
